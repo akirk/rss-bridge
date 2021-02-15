23c26ada8194ee9dc2085d3c32ec51702c9a51e9
Alex Kirk <alex.git@kirk.at>
2021-02-15 09:34:14 +0100

    [TwitterBridge] Fake Firefox user agent

--- bridges/TwitterBridge.php
+++ bridges/TwitterBridge.php
@@ -237,6 +237,7 @@ EOD
 	}
 
 	public function collectData(){
+		ini_set( 'user_agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:85.0) Gecko/20100101 Firefox/85.0' );
 		$html = '';
 		$page = $this->getURI();
 		$data = json_decode($this->getApiContents($this->getApiURI()));
